e906543f99dad517cf5410441aff3f653732cff6
Kevin Stokes <kevinstokes@Kevins-MacBook-Pro.local>
2019-04-14 17:29:21 -0400

    playlists change based on button

--- assets/js/spotify-search.js
+++ assets/js/spotify-search.js
@@ -3,7 +3,7 @@ var playlistId = "37i9dQZF1DXdPec7aLTmlC";
 
 $(document).on("click", "button", function () {
     console.log("You clicked me");
-    var accessToken = "BQAugtcY6N0ZFUpTWHXR43sYUViXGWmgASldTiVgoEeW25S0k-sVvMBXumddhCllWfjOvwubXCwAmc2TnKzqoV_-KomUFMWOdFYI002oXCJ5adMz5Bg8KozbCVuSrA93tECvjWY1eBcLRGnC";
+    var accessToken = "BQBB8NB3LxRWWuPu7Zz-Li4D84B0h8Ucxv5UjnAJdL4cycD0ITjl738rDxr6bqWslL8XWXOukq17jjdS_RCeeDxdTBjZbHQnF-DfGtEpm-J1coi4SDVtJJGBPKFAy0k7ZeL8ppzl4wojtGxI";
     var queryURL = "https://api.spotify.com/v1/playlists/37i9dQZF1DXdPec7aLTmlC"; 
 
     $.ajax({
