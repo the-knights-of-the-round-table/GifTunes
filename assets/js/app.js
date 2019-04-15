63470377d974c42b6440f72ea378fdc00f9846f8
Veronique Conus <veronique@Veroniques-MacBook-Pro.local>
2019-04-15 18:29:11 -0400

    stashing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,55 +1,58 @@
-//Create an array of strings with starter mood butons.
-$(document).ready(function() { 
-   
-    var moods = ["glad", "mad", "sad"];
-    
-    function displayGifRating() {
+//Create an array of strings, this will be our var=moods.
 
-        var gif = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=UxNR1uNih1F65bA3EEK3M4XZnDrOhr2A&limit=5";
-         
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-        .then(function(response) {
-            console.log(queryURL);
-            console.log(response.data);
+var moods = ["happy", "angry", "funny", "nervous", "romantic", "sad", "relax", "crazy", "confused", "tired" ];
+console.log(moods);
 
+//Take the moods in this array and create buttons in the HTML.
+
+function makeButtons () {
+    $("#addButton").empty(); //Use a  loop that appends a button for *each string* (by index) in the array.
+    for (var i = 0; i < moods.length; i++){ 
+
+    var button = $("<button>");
+    button.addClass("mood");
+    button.attr("data-name", moods[i]);
+    button.text(moods[i]);
+    $("#addButton").append(button);
+    }
+    getGifs();
+}
+
+//Make dem buttons!
+
+makeButtons();
+
+function getGifs() {
+    //When the user clicks  a button, the page should grab 5  animated gifs from the GIPHY API and place them on the page.
+
+    $('button').on('click', function() { 
+        var p = $(this).data('name');
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + p + "&api_key=Zr9700pOmpA44mJSPmhkDZFXmLkzWOk9&limit=5";
+
+        $.ajax({ url: queryURL, method: 'GET'})
+        .done(function(response) {
             var results = response.data;
-            for (var i = 0; i < results.length; i++){
-                var gifDiv = $('<div class= divGif>');
-                var showGif = $('<img>');
-                showGif.addClass("gifClick");
-                showGif.attr('src', results[i].images.fixed_height_animate.url);
-                showGif.attr('data-animate', results[i].images.fixed_height_animate.url);
-                showGif.attr('data-animate', results[i].images.fixed_height.url)
+            console.log(response);
+
+            //Create a loop to bring a rating and text into the div that holds the GIF
+            for (var i=0; i < results.length; i++) {
+
+                var gifDiv = $('<div class="item">'); //don't understand why this works but doesn't with ""
+                
+                var giphyImg = $("<img>");
+                giphyImg.attr("src", results[i].images.fixed_height_still.url);
+                giphyImg.attr("data-animate", results[i].images.fixed_height.url);
+
+                gifDiv.append(giphyImg)
+                gifDiv.append(p)
+
+                $("#gif-area").prepend(gifDiv);
             }
-        });
-    }
 
-    function makeButtons(){ 
-        $("#buttons-view").empty();
-        for (i = 0; i < moods.length; i++){
-            var a = $("<button>") 
-            a.addClass("gif-btn"); 
-            a.attr("data-name", moods[i]); 
-            a.text(moods[i]); 
-            $("#buttons-view").append(a);
-        }
-    }
-    // $(".gifClick").on("click", function(){
-        $(document).on("click", ".gifClick", function(){
-        console.log(this);
-        });
-
-    $("#add-gif").on("click", function() {
-        event.preventDefault();
-        var gif = $("#gif-input").val().trim();
-        moods.push(gif);
-        makeButtons();
-      });
-
-    $(document).on("click", ".gif-btn", displayGifRating);
-    makeButtons();
-})
+            $(".item").children("img").on("click", function() {
+                var state = $(this).attr("data-state")
+                }
+            });
+            });
+    });
+}
